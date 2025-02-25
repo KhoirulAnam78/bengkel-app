bf61c319765e64b5667e0387997b9cc57e2926cd
Khoirul Anam <92700194+KhoirulAnam78@users.noreply.github.com>
2025-02-25 14:57:52 +0700

    creta invoice number

--- app/Helper/GlobalHelper.php
+++ app/Helper/GlobalHelper.php
@@ -1,44 +1,40 @@
 <?php
-
 namespace App\Helper;
-use DateTime;
-use App\Models\Transaksi;
 
-class GlobalHelper  {
-    
-    static function generateInvoiceNumber($jenis):string
+use App\Models\Transaksi;
+use DateTime;
+
+class GlobalHelper
+{
+
+    public static function generateInvoiceNumber($jenis): string
     {
 
-       try{
+        try {
 
-        $current_date_time = new DateTime();
+            $current_date_time = new DateTime();
 
-        $date_sequence = $current_date_time->format("dmy");
+            $date_sequence = $current_date_time->format("dmy");
 
-        //section generate the sequence of running number of trip sheet
+            //section generate the sequence of running number of trip sheet
 
-        $lastTransactionId = Transaksi::orderBy('no_transaksi', 'desc')->where('jenis_transaksi',$jenis)->first();
+            $lastTransactionId = Transaksi::orderBy('no_transaksi', 'desc')->where('jenis_transaksi', $jenis)->first();
 
-        if (!$lastTransactionId)
-            // We get here if there is no TripSheet at all
-            // If there is no Trip sheet number set it to 0, which will be 1 at the end.
-            $number = 0;
-        else
-            $number = substr($lastTransactionId->no_transaksi, 9);
-        
-        if($jenis == 'keluar'){
-            return "IJM-".$date_sequence. sprintf('%03d', intval($number) + 1);
-        }else{
-            return "RES-".$date_sequence. sprintf('%03d', intval($number) + 1);
+            if (!$lastTransactionId) {
+                $number = 0;
+            } else {
+                $number = substr($lastTransactionId->no_transaksi, -1);
+            }
+
+            if ($jenis == 'keluar') {
+                return "IJM-" . $date_sequence . sprintf('%03d', intval($number) + 1);
+            } else {
+                return "RES-" . $date_sequence . sprintf('%03d', intval($number) + 1);
+            }
+
+        } catch (\Exception $e) {
+            dd($e);
         }
-            
-
-       }
-       catch (\Exception $e)
-       {
-           dd($e);
-       }
 
     }
 }
-?>
