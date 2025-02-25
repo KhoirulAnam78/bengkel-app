a453cb13211094214f1939f3fc556eceacc9e234
Khoirul Anam <92700194+KhoirulAnam78@users.noreply.github.com>
2025-02-25 17:26:17 +0700

    tryy

--- resources/views/transaksi/cetak-transaksi.blade.php
+++ resources/views/transaksi/cetak-transaksi.blade.php
@@ -1,11 +1,7 @@
 <!DOCTYPE html>
-<html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('receipt/fiddle.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('receipt/fiddle.css') }}"> --}}
     <title>Transaksi-{{ $transaksi->no_transaksi }}</title>
 </head>
 
@@ -74,7 +70,6 @@
     <script>
         window.print();
     </script>
-    <script src="{{ asset('receipt/script.js') }}"></script>
 </body>
 
 </html>
