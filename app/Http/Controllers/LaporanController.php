01e91e5469f9787214d11f55c6c7a326bacefc4a
Khoirul Anam <92700194+KhoirulAnam78@users.noreply.github.com>
2025-03-01 11:09:31 +0700

    route laporan

---
+++ app/Http/Controllers/LaporanController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Http\Controllers;
+
+class LaporanController extends Controller
+{
+    public function laporanPemasukan()
+    {
+
+    }
+
+    public function laporanPengeluaran()
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,20 @@
 <?php
 
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AjaxController;
-use App\Http\Controllers\RoleController;
-use App\Http\Controllers\TestController;
-use App\Http\Controllers\MitraController;
-use App\Http\Controllers\RouteController;
-use App\Http\Controllers\ProgramController;
-use App\Http\Controllers\RestockController;
-use App\Http\Controllers\SettingController;
-use App\Http\Controllers\AnalitikController;
-use App\Http\Controllers\MenuItemController;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\InstitusiController;
-use App\Http\Controllers\MenuGroupController;
-use App\Http\Controllers\PelatihanController;
-use App\Http\Controllers\SparepartController;
-use App\Http\Controllers\TransaksiController;
+use App\Http\Controllers\LaporanController;
 use App\Http\Controllers\MasterDataController;
+use App\Http\Controllers\MenuGroupController;
+use App\Http\Controllers\MenuItemController;
 use App\Http\Controllers\PermissionController;
-use App\Http\Controllers\RepositoriController;
-use App\Http\Controllers\SumberDanaController;
-use App\Http\Controllers\UserProfileController;
-use App\Http\Controllers\KriteriaMitraController;
-use App\Http\Controllers\BentukKegiatanController;
+use App\Http\Controllers\RestockController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\RouteController;
+use App\Http\Controllers\SettingController;
+use App\Http\Controllers\TransaksiController;
 use App\Http\Controllers\UserManagementController;
-use App\Http\Controllers\StatusKerjasamaController;
-
+use App\Http\Controllers\UserProfileController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -53,37 +40,40 @@ Route::group(['middleware' => ['web', 'auth', 'verified']], function () {
     Route::get('dashboard/mode', [DashboardController::class, 'mode'])->name('dashboard.mode');
 
     // DATA MASTER
-    Route::get('data-jenis-motor', [MasterDataController::class,'jenisMotor'])->name('jenis-motor.index');
-    Route::get('data-mekanik', [MasterDataController::class,'mekanik'])->name('mekanik.index');
-    Route::get('data-spareparts', [MasterDataController::class,'sparepart'])->name('sparepart.index');
-    Route::get('data-suppliers', [MasterDataController::class,'suppliers'])->name('suppliers.index');
-    Route::get('data-services', [MasterDataController::class,'services'])->name('services.index');
- 
+    Route::get('data-jenis-motor', [MasterDataController::class, 'jenisMotor'])->name('jenis-motor.index');
+    Route::get('data-mekanik', [MasterDataController::class, 'mekanik'])->name('mekanik.index');
+    Route::get('data-spareparts', [MasterDataController::class, 'sparepart'])->name('sparepart.index');
+    Route::get('data-suppliers', [MasterDataController::class, 'suppliers'])->name('suppliers.index');
+    Route::get('data-services', [MasterDataController::class, 'services'])->name('services.index');
+
     // TRANSAKSI
-    Route::get('transaksi-bengkel',[TransaksiController::class,'transaksi'])->name('transaksi.bengkel');
-    Route::get('riwayat-transaksi-bengkel',[TransaksiController::class,'riwayat'])->name('transaksi.riwayat');
+    Route::get('transaksi-bengkel', [TransaksiController::class, 'transaksi'])->name('transaksi.bengkel');
+    Route::get('riwayat-transaksi-bengkel', [TransaksiController::class, 'riwayat'])->name('transaksi.riwayat');
     Route::get('cetak-transaksi-bengkel/{id}', [TransaksiController::class, 'cetak'])->name('transaksi.cetak');
 
     // RESTOCK
-    Route::get('restock-barang', [RestockController::class,'index'])->name('restock.index');
-    Route::get('riwayat-restock-barang', [RestockController::class,'riwayat'])->name('restock.riwayat');
+    Route::get('restock-barang', [RestockController::class, 'index'])->name('restock.index');
+    Route::get('riwayat-restock-barang', [RestockController::class, 'riwayat'])->name('restock.riwayat');
+
+    // Laporan
+    Route::get('laporan-pemasukan', [LaporanController::class, 'laporanPemasukan'])->name('laporan_pemasukan');
+    Route::get('laporan-pengeluaran', [LaporanController::class, 'laporanPengeluaran'])->name('laporan_pengeluaran');
 
     // Pengaturan
-    Route::get('setting', [SettingController::class,'index'])->name('setting.index');
+    Route::get('setting', [SettingController::class, 'index'])->name('setting.index');
     Route::post('setting/update', [SettingController::class, 'update'])->name('setting.update');
 
     // REF DATA AJAX
-    Route::get('referensi-sparepart', [AjaxController::class,'sparepart'])->name('ajax.sparepart');
-    Route::get('referensi-service', [AjaxController::class,'service'])->name('ajax.service');
-    Route::get('referensi-mekanik', [AjaxController::class,'mekanik'])->name('ajax.mekanik');
-    Route::get('referensi-supplier', [AjaxController::class,'supplier'])->name('ajax.supplier');
+    Route::get('referensi-sparepart', [AjaxController::class, 'sparepart'])->name('ajax.sparepart');
+    Route::get('referensi-service', [AjaxController::class, 'service'])->name('ajax.service');
+    Route::get('referensi-mekanik', [AjaxController::class, 'mekanik'])->name('ajax.mekanik');
+    Route::get('referensi-supplier', [AjaxController::class, 'supplier'])->name('ajax.supplier');
 
     // ROUTE DEVELOPER
     Route::resource('user', UserManagementController::class)->only('index', 'store', 'update', 'destroy');
     Route::prefix('user')->group(function () {
         Route::resource('profile', UserProfileController::class)->only('index');
     });
-    
 
     Route::resource('route', RouteController::class)->only('index', 'store', 'update', 'destroy');
     Route::resource('role', RoleController::class)->only('index', 'store', 'update', 'destroy');
